8a044b5f3ca9e4a4964af9927a387d888f92fd09
fajarfadillahh <fajaragustian979@gmail.com>
2023-09-01 11:39:22 +0700

    modified Home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import Image from "next/image";
+import { HiArrowRight } from "react-icons/hi";
 
 // import material-components
 import { Button } from "@material-tailwind/react";
@@ -18,7 +20,44 @@ export default function Home() {
 
       <Layout>
         {/* ==== hero section ==== */}
-        <section className="pb-16 pt-32">hero section</section>
+        <section className="pb-16 pt-40">
+          <div className="container grid grid-cols-2 items-center gap-8">
+            <div>
+              <h1 className="section-title max-w-[520px] text-[48px]">
+                Tentukan pilihan kamu dari sekarang. 🚀
+              </h1>
+              <p className="section-text max-w-[490px] pb-8">
+                Ponic adalah platform voting berbasis web yang inovatif dan
+                user-friendly yang dirancang untuk memenuhi segala jenis
+                kebutuhan pemungutan suara anda.
+              </p>
+              <div className="inline-flex items-center gap-3">
+                <Button
+                  size="lg"
+                  color="pink"
+                  className="h-[48px] text-base font-bold capitalize"
+                >
+                  Buat voting
+                </Button>
+                <Button
+                  size="lg"
+                  variant="text"
+                  className="inline-flex h-[48px] items-center gap-1 text-base font-bold capitalize text-pink-500"
+                >
+                  Halaman dashboard
+                  <HiArrowRight />
+                </Button>
+              </div>
+            </div>
+
+            <Image
+              src="/assets/img-1.svg"
+              alt="hero img"
+              width={580}
+              height={475}
+            />
+          </div>
+        </section>
       </Layout>
     </>
   );
