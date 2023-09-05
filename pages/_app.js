c8675a2fd21c9f86808e425144dd7c5701007f4e
gufronnakaaw <gufronnakaaw@gmail.com>
2023-09-05 13:45:17 +0700

    create global event to check token

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,29 @@ import "@/styles/globals.css";
 
 import NextNProgress from "nextjs-progressbar";
 
+// import utils
+import { useEffect } from "react";
+import Cookies from "js-cookie";
+import { useRouter } from "next/router";
+
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (
+      router.pathname.startsWith("/dashboard") ||
+      router.pathname.startsWith("/rooms")
+    ) {
+      window.addEventListener("focus", () => {
+        const token = Cookies.get("token");
+
+        if (!token) {
+          return (window.location.href = "/auth/login");
+        }
+      });
+    }
+  }, [router.pathname]);
+
   return (
     <>
       <NextNProgress color="#E43F6F" />
