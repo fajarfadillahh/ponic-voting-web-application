0390846106a1e00e0e29d84fc9b5864c3ebd6511
fajarfadillahh <fajaragustian979@gmail.com>
2023-09-11 16:21:32 +0700

    modified Footer components & create button for dark mode feature

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,17 +1,32 @@
-// import materail-components
-import { Typography } from "@material-tailwind/react";
+import { HiOutlineSun, HiOutlineMoon } from "react-icons/hi";
+import { IconButton, Typography } from "@material-tailwind/react";
 
 export default function Footer() {
   return (
     <footer className="mx-auto mt-8 max-w-[1440px]">
-      <div className="container flex h-24 items-center justify-center border-t-2 border-gray-200">
+      <div className="container flex flex-col items-center gap-4 border-t-2 border-gray-200 py-8 text-center md:flex-row md:justify-between md:text-left">
         <Typography
           variant="paragraph"
           color="blue-gray"
           className="font-semibold"
         >
-          Hak cipta {new Date().getFullYear()} - Di kembangkan oleh ❤️
+          &copy; Hak cipta {new Date().getFullYear()} Ponic. - Di kembangkan
+          oleh <span className="font-extrabold text-pink-500">Fglabs.</span> ❤️
         </Typography>
+
+        <div className="inline-flex items-center gap-2">
+          <Typography
+            variant="paragraph"
+            color="blue-gray"
+            className="font-semibold"
+          >
+            Mode gelap
+          </Typography>
+
+          <IconButton size="sm" variant="outlined" className="text-[1rem]">
+            <HiOutlineMoon />
+          </IconButton>
+        </div>
       </div>
     </footer>
   );
